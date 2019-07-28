5cd3998a32e68f4b68a7697fa642c1cad6d84f11
RyanBoring <boring.ryan.alan@gmail.com>
2019-07-28 13:06:29 -0400

    Remove clutter

--- CMakeFiles/DeepSpaceVision.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "DeepSpaceVision.pdb"
-  "DeepSpaceVision"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DeepSpaceVision.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
