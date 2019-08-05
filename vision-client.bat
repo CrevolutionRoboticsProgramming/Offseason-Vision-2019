644645d534b51a6bd59c77f33e02eb97286b38bd
RyanBoring <boring.ryan.alan@gmail.com>
2019-08-05 06:20:35 -0400

    Add infinite loops to clients

--- vision-client.bat
+++ vision-client.bat
@@ -1,1 +1,3 @@
-C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e gst-launch-1.0 udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
+:start
+    C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e gst-launch-1.0 udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
+    goto :start
--- vision-client.sh
+++ vision-client.sh
@@ -1,1 +1,4 @@
-gst-launch-1.0 udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
+while true
+do
+    gst-launch-1.0 udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
+done
