d82e1bf00e5a2c2bbeb30020790d33cf9fd91aed
RyanBoring <boring.ryan.alan@gmail.com>
2019-08-05 10:07:18 -0400

    Fix vision-client.bat

--- vision-client.bat
+++ vision-client.bat
@@ -1,3 +1,3 @@
 :start
-    C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e gst-launch-1.0 udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
+    C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -e udpsrc port=1181 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
     goto :start
